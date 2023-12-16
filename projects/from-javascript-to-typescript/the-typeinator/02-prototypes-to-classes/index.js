20fec74f6eb28a8a9499c85f4727358feea9019e
fiosman <fares.osman@outlook.com>
2023-12-16 17:21:43 -0500

    adds progress to refactor

--- projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
@@ -1,5 +1,26 @@
 // Put your Robot and Humanoid classes here! ✨
 // See ./original.js for their older JavaScript code.
 
+class Robot {
+	constructor(name, abilities) {
+		this.name = name;
+		this.abilities = abilities;
+		this.power = 100;
+	}
+
+	announce = () => {
+		console.log("Greetings. I am " + this.name + ".");
+
+		for (const ability of this.abilities) {
+			console.log("I am able to " + this.abilities[i] + ".");
+		}
+	};
+
+	charge = (amount) => {};
+}
+
+module.exports.Humanoid = Humanoid;
+module.exports.Robot = Robot;
+
 module.exports.Humanoid = Humanoid;
 module.exports.Robot = Robot;
